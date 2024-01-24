c4babef906ad88bbe8d0530d5409def5f22f1310
Ritvik Upadhyay <ritvikupadhyay2000@gmail.com>
2024-01-24 15:17:51 +0530

    Add views and urls to blog app. Updated the urls in main to reflect blog urls'

--- blog/urls.py
+++ blog/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.starting_page),
+    path('/posts',views.posts),
+    path('/posts/<slug:slug>', views.post_detail)
+]
